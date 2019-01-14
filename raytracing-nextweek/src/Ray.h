827c7b0a55d54f6113ee9e1c5382141c4b4fbd21
osozero <sametozer13@gmail.com>
2019-01-13 21:31:12 -0800

    ray added.

--- raytracing-nextweek/src/Ray.h
+++ raytracing-nextweek/src/Ray.h
@@ -1,8 +1,38 @@
 #pragma once
-class Ray
+#include "glm/vec3.hpp"
+
+class ray
 {
 public:
-	Ray();
-	~Ray();
+	ray();
+	ray(const glm::vec3 a, const glm::vec3 &b, float f = 0.0)
+		:origin(a), direction(b), _time(f){}
+	
+	glm::vec3 getOrigin() const
+	{
+		return origin;
+	}
+
+	glm::vec3 getDirection() const
+	{
+		return direction;
+	}
+
+	float getTime() const
+	{
+		return _time;
+	}
+
+	glm::vec3 pointAtParameter(float t)
+	{
+		return origin + t * direction;
+	}
+
+	~ray();
+
+private:
+	glm::vec3 origin;
+	glm::vec3 direction;
+	float _time;
 };
 
