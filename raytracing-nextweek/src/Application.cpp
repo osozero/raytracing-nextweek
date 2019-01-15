c91760381177a9bb68596ba78a601cd61ce79240
osozero <sametozer13@gmail.com>
2019-01-15 00:26:19 -0800

    psuedo anti aliasing added to main loop.

--- raytracing-nextweek/src/Application.cpp
+++ raytracing-nextweek/src/Application.cpp
@@ -5,22 +5,24 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
+#include <random>
 
 
 class hitable;
 
-glm::vec3 color(const ray &r,hitable *world)
+glm::vec3 color(const ray& r, hitable* world)
 {
 	hitRecord record;
 
-	if(world->hit(r,0.0, std::numeric_limits<float>::max(),record))
+	if (world->hit(r, 0.0, std::numeric_limits<float>::max(), record))
 	{
-		return 0.5f*glm::vec3(record.normal.x + 1, record.normal.y + 1, record.normal.z + 1);
-	}else
+		return 0.5f * glm::vec3(record.normal.x + 1, record.normal.y + 1, record.normal.z + 1);
+	}
+	else
 	{
 		glm::vec3 direction = glm::normalize(r.getDirection());
 		float f = 0.5f * (direction.y + 1.0);
-		return (1.0f-f)*glm::vec3(1.0,1.0,1.0) + f*glm::vec3(0.5,0.7,1.0);
+		return (1.0f - f) * glm::vec3(1.0, 1.0, 1.0) + f * glm::vec3(0.5, 0.7, 1.0);
 	}
 }
 
@@ -29,32 +31,43 @@ int main()
 	int x = 200;
 	int y = 100;
 
+	int aa = 100;
+
 	std::cout << "P3\n" << x << " " << y << "\n255\n";
 
 	camera cam;
 
-	hitable *list[2];
-	
+	hitable* list[2];
+
 	list[0] = new sphere(glm::vec3(0.0, 0.0, -1.0), 0.5);
 	list[1] = new sphere(glm::vec3(0.0, -100.5, -1.0), 100.0);
 
-	hitable *world = new hitable_list(list, 2);
+	hitable* world = new hitable_list(list, 2);
 
-	for(int j=y-1;j>=0;j--)
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+	for (int j = y - 1; j >= 0; j--)
 	{
-		for(int i=0;i<x;i++)
+		for (int i = 0; i < x; i++)
 		{
-			float u = float(i) / float(x);
-			float v = float(j) / float(y);
+			glm::vec3 col(0, 0, 0);
+			for (int k = 0; k < aa; k++)
+			{
+				float u = float(i + distribution(generator)) / float(x);
+				float v = float(j + distribution(generator)) / float(y);
 
-			ray r = cam.getRay(u, v);
+				ray r = cam.getRay(u, v);
 
-			glm::vec3 p = r.pointAtParameter(2.0);
-			glm::vec3 col = color(r, world);
+				glm::vec3 p = r.pointAtParameter(2.0);
+				col += color(r, world);
+			}
 
-			int ir = int(255.99*col[0]);
-			int ig = int(255.99*col[1]);
-			int ib = int(255.99*col[2]);
+			col /= float(aa);
+
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
